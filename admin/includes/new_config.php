80eb120dddc458e3c673231d0055b94babac9fc6
Orxan Axmdov <baku_orxan17@mail.ru>
2017-12-11 01:07:10 +0400

    user.php

---
+++ admin/includes/new_config.php
@@ -0,0 +1,13 @@
+<?php 
+
+
+
+$db['db_host'] = "localhost";
+$db['db_user'] = "root";
+$db['db_pass'] = "";
+$db['db_name'] = "gallery";
+
+foreach ($db as $key => $value) {
+	define(strtoupper($key), $value);
+}
+?>
---
+++ admin/includes/user.php
@@ -0,0 +1,67 @@
+<?php 
+
+class User {
+	public $id;
+	public $username;
+	public $password;
+	public $first_name;
+	public $last_name;
+
+
+	public static function find_all_users(){
+		return self::find_this_query("SELECT * from users");
+	}
+
+	public static function find_user_by_id($user_id){
+		global $database;
+
+		$the_result_array = self::find_this_query("SELECT * from users where id = $user_id limit 1");
+				
+		return !empty($the_result_array) ? array_shift($the_result_array) : false;
+	}
+
+	public static function find_this_query($sql){
+		global $database;
+		$result_set = $database->query($sql);
+		$the_object_array = array();
+
+		while ($row = mysqli_fetch_array($result_set)) {
+			$the_object_array[] = self::instantation($row);
+		}
+
+		return $the_object_array;
+	}
+
+
+// $the_object->id 				= $found_user['id'];
+    // $the_object->username 	= $found_user['username'];
+    // $the_object->password 	= $found_user['password'];
+    // $the_object->first_name = $found_user['firstname'];
+    // $the_object->last_name  = $found_user['lastname'];
+
+	public static function instantation($the_record){
+
+		$the_object = new self;
+
+    
+		foreach ($the_record as $the_attribute => $value) {
+
+			if($the_object->has_the_attribute($the_attribute)){
+
+				$the_object->$the_attribute = $value;
+
+			}
+
+		}
+
+    return $the_object;
+	}
+
+	private function has_the_attribute($the_attribute){
+		$object_properties = get_object_vars($this);
+
+		return array_key_exists($the_attribute, $object_properties);
+	}
+}
+
+ ?>
